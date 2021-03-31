5dae5790630ea6cc1615f8b02fa1a1a8d9ec4970
Alexmanger2 <Alexmanger2@gmail.com>
2021-03-31 17:22:40 -0400

    reg update

--- src/project/Registration.java
+++ src/project/Registration.java
@@ -1,104 +1,137 @@
 package project;
 
+
 import java.util.Calendar;
 
 public class Registration {
 
-	private Person membershipType;
-
-//	private boolean validCard;
-	private boolean hasCard = false;
-	private boolean expired = false;
-
-	private int issuedDate;
-	private int currentDate;
-	private String name;
-	private String num;
-
-	/** Default constructor for Registration
-	 *  Does Nothing
-	 */
+	
+	private Person membershipType; //type of member
+	
+	private boolean hasCard = false;  //valid card or not
+	private boolean expired = false; //expiration date
+	
+	private int issuedDate;  //date card was issued
+	private int currentDate; // todays date
+	
+	private String name; // name of person
+	private String num;  // phone number
+	private String type; // child/adult
+	
+	
 	public Registration() {
-
+		
 	}
-
+	
+	/**
+	 * Checks to see if library card has expired or not
+	 * 
+	 * */
 	public void checkCard() {
-
+		
 		checkExpired();
-
-		if (membershipType == null || expired == true || hasCard == false) {
-			// this.validCard = false;
+		
+		if(membershipType == null || expired == true || hasCard == false ) 
 			System.out.println("No Library card found");
-		} else {
-			// this.validCard = true;
+		else 
 			System.out.println("Library card found");
-		}
-
+		
+		
 	}
-
-	// private
-	private void checkExpired() {
+	
+	/**
+	 * 
+	 * private method that does calculations for expiration date
+	 * 
+	 * */
+	private void checkExpired() {   
 		todayDate();
 
-		if (this.currentDate - this.issuedDate > 3) {
+		if(this.currentDate - this.issuedDate > 3) {
 			this.expired = true;
-			// System.out.println("Library card has expired");
-			// System.out.println(this.currentDate - this.issuedDate);
 		}
-		// System.out.println("Library card has NOT expired");
+	
 	}
-
+	
+	/**
+	 * Registers the person for a library card. 
+	 * Records the time/date of registration
+	 * Checks whether person is adult or child
+	 *	
+	 * @param Person class instance
+	 * */
+	
 	public void register(Person type) {
-
+		
 		this.membershipType = type;
 		this.name = type.getFirstName() + " " + type.getLastName();
 		this.num = type.getPhoneNumber();
-
+		
 		Calendar registered = Calendar.getInstance();
 		this.issuedDate = registered.get(Calendar.YEAR);
 		this.hasCard = true;
-
-//		cal1.set(Calendar.YEAR, 2020); 
-//		this.iss = cal1.get(Calendar.YEAR);
-
+		
+		int typeAge = this.getIssuedDate() -type.getBirthYear();
+		if(typeAge >= 18)
+			this.type = "Adult";
+		else
+			this.type = "Child";
+		
 	}
-
+	
+	
+	/**
+	 * 
+	 *  records todays date
+	 * 
+	 * */
+	
 	public void todayDate() {
-
+		
 		Calendar date = Calendar.getInstance();
 		this.currentDate = date.get(Calendar.YEAR);
 	}
 
+	
+	/**
+	 * Gets issued date
+	 * @return date the library card was issued
+	 * */
+	
 	public int getIssuedDate() {
-
+		
 		return this.issuedDate;
 	}
-
+	
+	
+	/**
+	 * 
+	 * Displays all the information about the person who registered
+	 * for a library card in a neat fashion
+	 * 
+	 * */
 	public void displayCardInfo() {
+		
 		System.out.println();
-		// System.out.println("********* Card Information *********");
 		System.out.printf("%24s \n\n", "Card Information");
-		// System.out.println("Name: " + this.name);
-
-		System.out.printf("%13s %17s \n", "Name:", this.name);
-		// System.out.println("Number: " + this.num);
-		System.out.printf("%15s %14s \n", "Number:", this.num);
-//		System.out.println("Date issued: " + getIssuedDate());
-		System.out.printf("%15s %6d \n", "Issued:", getIssuedDate());
-//		System.out.println("Expiration date: " + (getIssuedDate() + 3) + "\n");
-		System.out.printf("%17s %4d \n", "Exp Date:", (getIssuedDate() + 3));
-
+		System.out.printf("%13s %17s \n", "Name:" , this.name);
+		System.out.printf("%15s %14s \n", "Number:" , this.num);
+		System.out.printf("%15s %6d \n", "Issued:", getIssuedDate() );
+		System.out.printf("%17s %4d \n", "Exp Date:", (getIssuedDate() + 3) );
+		System.out.printf("%13s %9s \n", "Type:", this.type );
 	}
-
+	
+	
 	public static void main(String[] args) {
-
+	
 		Registration tester = new Registration();
 		Person matt = new Person("Matthew", "Smith", 1999);
-		Person john = new Person("John", "Doe", 1954);
 		matt.setPhoneNumber("555-444-4534");
 		tester.register(matt);
 		tester.checkCard();
 		tester.displayCardInfo();
+		
+		
 		
 	}
 
