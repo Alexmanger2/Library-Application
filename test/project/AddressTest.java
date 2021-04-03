bdeeda087ee3e6e7e2fa35e70c6f3311e91ddbf9
Daniel Targonski <daniel.targonski2@gmail.com>
2021-04-03 00:36:19 -0400

    setup() method added to AddressTest

--- test/project/AddressTest.java
+++ test/project/AddressTest.java
@@ -6,60 +6,70 @@ import org.junit.jupiter.api.Test;
 
 class AddressTest {
 
+	private Address address;
+	private Address address2;
+	private Address address3;
+
+	void setup() {
+		address = new Address("testStreet", "testCity", "testZip", "testState");
+//		address2 = new Address("testStreet", "testCity", "testZip", "testState");
+//		address3 = new Address("testStreet3", "testCity3", "testZip3", "testState3");
+	}
+
 	@Test
 	void testAddressStringStringStringString() {
-	Address address = new Address("testStreet", "testCity", "testZip", "testState");
-	Address address2 = new Address("testStreet", "testCity", "testZip", "testState");
-	Address address3 = new Address("testStreet3", "testCity3", "testZip3", "testState3");
-	
-	assertEquals(address, address2);
-	assertNotEquals(address, address3);
-	
-	assertEquals("testStreet", address.getStreet());
-	assertEquals("testCity", address.getCity());
-	assertEquals("testZip", address.getZip());
-	assertEquals("testState", address.getState());
-	
+		address = new Address("testStreet", "testCity", "testZip", "testState");
+		address2 = new Address("testStreet", "testCity", "testZip", "testState");
+		address3 = new Address("testStreet3", "testCity3", "testZip3", "testState3");
+
+		assertEquals(address, address2);
+		assertNotEquals(address, address3);
+
+		assertEquals("testStreet", address.getStreet());
+		assertEquals("testCity", address.getCity());
+		assertEquals("testZip", address.getZip());
+		assertEquals("testState", address.getState());
+
+		assertTrue(address.getCity() instanceof String);
+
 	}
 
 	@Test
 	void testSetStreet() {
-		Address address = new Address();
+		setup();
 		address.setStreet("testStreet");
-		
 		assertEquals("testStreet", address.getStreet());
 	}
 
 	@Test
 	void testSetCity() {
-		Address address = new Address();
+		setup();
 		address.setCity("testCity");
 		assertEquals("testCity", address.getCity());
 	}
 
 	@Test
 	void testSetZip() {
-		Address address = new Address();
+		setup();
 		address.setZip("12345");
 		assertEquals("12345", address.getZip());
 	}
 
 	@Test
 	void testSetState() {
-		Address address = new Address();
+		setup();
 		address.setState("NY");
 		assertEquals("NY", address.getState());
 	}
 
 	@Test
 	void testToString() {
-		Address address = new Address("testStreet", "testCity", "testZip", "testState");
-		
-		String testString = "Address [street=" + address.getStreet() + ", city=" + address.getCity() + 
-				", zip=" + address.getZip() + ", state=" + address.getState() + "]";
-		
+		setup();
+		String testString = "Address [street=" + address.getStreet() + ", city=" + address.getCity() + ", zip="
+				+ address.getZip() + ", state=" + address.getState() + "]";
+
 		assertEquals(address.toString(), testString);
-		
+
 	}
 
 }
---
+++ test/project/RegistrationTest.java
@@ -0,0 +1,39 @@
+package project;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RegistrationTest {
+
+	@Test
+	void testRegistration() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCheckCard() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRegister() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testTodayDate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetIssuedDate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDisplayCardInfo() {
+		fail("Not yet implemented");
+	}
+
+}
